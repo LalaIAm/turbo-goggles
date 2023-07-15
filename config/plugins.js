5b8cedb0ffde3de8dae09ea4acb84355bfd6da09
LalaIAm <lthorud12@gmail.com>
2023-07-15 01:05:04 -0500

    update docs

--- config/plugins.js
+++ config/plugins.js
@@ -14,31 +14,34 @@ module.exports = ({ env }) => ({
       },
     },
   },
-    documentation: {
-        enabled: true,
-        config: {
-            openapi: "3.0.0",
-            info: {
-                version: "1.0.0",
-                title: "TradeMe API",
-                description: "A bartering API",
-                termsOfService: "",
-                contact: {
-                    name: "LalaIAm",
-                    email: "lthorud12@gmail.com",
-                    url: "https://github.com/lalaiam",
-                },
-                licenses: {
-                    name: "Apache 2.0",
-                    url: "https://www.apache.org/licenses/LICENSE-2.0.html",
-                },
-            },
-            'x-strapi-config': {
-                plugins: ['upload', 'users-permissions'],
-                path: '/documentation'
-            },
-            servers: [{ url: 'http://localhost:1337/api', description: 'Local Dev Server' }],
-            security: [{ bearerAuth: [] }]
+  documentation: {
+    enabled: true,
+    config: {
+      openapi: "3.0.0",
+      info: {
+        version: "1.0.0",
+        title: "TradeMe API",
+        description: "A bartering API",
+        termsOfService: "",
+        contact: {
+          name: "LalaIAm",
+          email: "lthorud12@gmail.com",
+          url: "https://github.com/lalaiam",
         },
-    }
+        licenses: {
+          name: "Apache 2.0",
+          url: "https://www.apache.org/licenses/LICENSE-2.0.html",
+        },
+      },
+      "x-strapi-config": {
+        plugins: ["upload", "users-permissions"],
+        path: "/documentation",
+      },
+      servers: [
+        { url: "http://localhost:1337/api", description: "Local Dev Server" },
+        { url: "admin--trade-server--wbl2nxkv68w7.code.run/api", description: 'Web Server' },
+      ],
+      security: [{ bearerAuth: [] }],
+    },
+  },
 });
